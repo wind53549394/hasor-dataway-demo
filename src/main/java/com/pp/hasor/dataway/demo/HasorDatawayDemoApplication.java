a85a916f3f5427af1fe70df245d1d28a087b050e
wind53549394 <1243740490@qq.com>
2020-04-18 21:14:10 +0800

    整合提交Demo

---
+++ src/main/java/com/pp/hasor/dataway/demo/HasorDatawayDemoApplication.java
@@ -0,0 +1,30 @@
+package com.pp.hasor.dataway.demo;
+
+import net.hasor.spring.boot.EnableHasor;
+import net.hasor.spring.boot.EnableHasorWeb;
+import net.hasor.web.startup.RuntimeFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@EnableHasor()
+@EnableHasorWeb()
+@SpringBootApplication
+public class HasorDatawayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HasorDatawayDemoApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean runtimeFilterRegistration(){
+        FilterRegistrationBean registration = new FilterRegistrationBean(new RuntimeFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("runtimeFilter");
+        registration.setOrder(1);
+        return registration;
+
+    }
+}
---
+++ src/main/java/com/pp/hasor/dataway/demo/module/ExampleModule.java
@@ -0,0 +1,27 @@
+package com.pp.hasor.dataway.demo.module;
+
+import lombok.extern.slf4j.Slf4j;
+import net.hasor.core.ApiBinder;
+import net.hasor.core.DimModule;
+import net.hasor.db.JdbcModule;
+import net.hasor.db.Level;
+import net.hasor.spring.SpringModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@DimModule
+@Component
+public class ExampleModule implements SpringModule {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void loadModule(ApiBinder apiBinder) throws Throwable {
+        // DataSource from Spring boot into Hasor
+        apiBinder.installModule(new JdbcModule(Level.Full, this.dataSource));
+    }
+}
