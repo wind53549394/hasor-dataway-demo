7f6bcbc6da4d09454240e03282c46d630df1a874
wind53549394 <1243740490@qq.com>
2020-04-19 11:05:13 +0800

    整合Freemarker

---
+++ src/main/java/com/pp/hasor/dataway/demo/controller/FreemarkerController.java
@@ -0,0 +1,15 @@
+package com.pp.hasor.dataway.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FreemarkerController {
+
+    @GetMapping("index")
+    public String index(Model model){
+        model.addAttribute("content","Hello World");
+        return "index";
+    }
+}
